e7eefd4306e63aa004a0de83c73dee7420f5891f
Rajeev Menon <rajeevrmenon97@gmail.com>
2025-05-05 04:45:56 -0700

    Add plymouth splash

--- hosts/laptop-nixos/configuration.nix
+++ hosts/laptop-nixos/configuration.nix
@@ -14,6 +14,7 @@
     ../../nixos/ios.nix
     ../../nixos/media.nix
     ../../nixos/nvidia.nix
+    ../../nixos/plymouth.nix
     ../../nixos/secureboot.nix
     ../../nixos/users.nix
   ];
---
+++ nixos/plymouth.nix
@@ -0,0 +1,18 @@
+{
+  boot = {
+    plymouth = {
+      enable = true;
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+  };
+}
