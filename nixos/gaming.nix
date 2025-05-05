d09d0bfc66075b713c8ca618d19cebd0dcaf72f6
Rajeev Menon <rajeevrmenon97@gmail.com>
2025-05-05 13:12:22 -0700

    Add gamemode

--- nixos/gaming.nix
+++ nixos/gaming.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+    gamemode.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
