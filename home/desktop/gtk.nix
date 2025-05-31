891605da50627da69a12488faa289defa0fa5290
Rajeev Menon <rajeevrmenon97@gmail.com>
2025-05-31 05:32:37 -0700

    Fluent theme for qt and gtk

--- home/desktop/gtk.nix
+++ home/desktop/gtk.nix
@@ -10,17 +10,24 @@
   gtk = {
     enable = true;
     theme = {
-      package = pkgs.kdePackages.breeze-gtk;
-      name = "Breeze-Dark";
+      package = pkgs.fluent-gtk-theme;
+      name = "Fluent-Dark";
     };
 
     iconTheme = {
       package = pkgs.kdePackages.breeze-icons;
       name = "breeze-dark";
     };
+
+    font = {
+      package = pkgs.noto-fonts;
+      name = "Noto Sans";
+      size = 10;
+    };
   };
 
   home.packages = with pkgs; [
     nwg-look
+    fluent-gtk-theme
   ];
 }
--- home/desktop/qt.nix
+++ home/desktop/qt.nix
@@ -1,25 +1,53 @@
-{pkgs, ...}: {
+{pkgs, ...}: 
+let 
+  fluent-kde = pkgs.stdenv.mkDerivation {
+    pname = "fluent-kvantum";
+    version = "latest";
+    src = pkgs.fetchFromGitHub {
+      owner = "vinceliuice";
+      repo = "Fluent-kde";
+      rev = "315251f3e0099fc7afa7ab183880141a478f584e";
+      hash = "sha256-DBliFDKSAEFWqKtGvOzwKxNi3YK5VEbLzKa8byEUg1Y=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/share/Kvantum
+      mkdir -p $out/share/color-schemes
+      mkdir -p $out/share/aurorae/themes
+      mkdir -p $out/share/plasma/desktoptheme
+      mkdir -p $out/share/plasma/look-and-feel
+      mkdir -p $out/share/wallpapers
+      mkdir -p $out/share/plasma/plasmoids
+      mkdir -p $out/share/plasma/layout-templates
+
+      cp -r Kvantum/* $out/share/Kvantum/
+      cp -r aurorae/* $out/share/aurorae/themes/
+      cp -r plasma/desktoptheme/* $out/share/plasma/desktoptheme/
+      cp -r plasma/look-and-feel/* $out/share/plasma/look-and-feel/
+      cp -r wallpaper/* $out/share/wallpapers/
+      cp -r color-schemes/*.colors $out/share/color-schemes/
+      cp -r plasma/plasmoids/* $out/share/plasma/plasmoids/ || true
+      cp -r plasma/layout-templates/* $out/share/plasma/layout-templates/ || true
+    '';
+  };
+in {
+  home.packages = with pkgs; [
+    kdePackages.qtstyleplugin-kvantum
+    fluent-kde
+  ];
+
   qt = {
     enable = true;
-    platformTheme.name = "qt5ct";
-    style.name = "Breeze";
+    platformTheme.name = "kde";
+    style.name = "kvantum";
   };
-  xdg.configFile."qt5ct/colors.conf".text = ''
-    [ColorScheme]
-    name=BreezeDark
-  '';
 
-  xdg.configFile."qt5ct/qt5ct.conf".text = ''
-    [Appearance]
-    style=Breeze
-    color_scheme_path=${pkgs.libsForQt5.breeze-qt5}/share/color-schemes/BreezeDark.colors
-  '';
-  home.packages = with pkgs; [
-    libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.breeze-qt5
-    libsForQt5.qt5ct
-  ];
-  home.sessionVariables = {
-    QT_QPA_PLATFORMTHEME = "qt5ct";
+  xdg.configFile = {
+    "Kvantum/FluentDark/FluentDark.kvconfig".source = "${fluent-kde}/share/Kvantum/Fluent/FluentDark.kvconfig";
+    "Kvantum/FluentDark/FluentDark.svg".source = "${fluent-kde}/share/Kvantum/Fluent/FluentDark.svg";
+    "Kvantum/kvantum.kvconfig".text = ''
+      [General]
+      theme=FluentDark
+    '';
   };
 }
