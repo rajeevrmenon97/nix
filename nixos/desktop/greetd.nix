638917e2d13b5ac5869bdf5be4223c2edced7316
Rajeev Menon <rajeevrmenon97@gmail.com>
2025-06-07 06:28:00 -0700

    Show asterisks in greetd

--- nixos/desktop/greetd.nix
+++ nixos/desktop/greetd.nix
@@ -4,7 +4,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --asterisks --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions --remember --remember-user-session";
         user = "greeter";
       };
     };
