555e1bba3778f39867f37f60370ffa61c3984cd3
Rajeev Menon <rajeevrmenon97@gmail.com>
2025-06-07 06:39:26 -0700

    Use cachix cache

--- nixos/system/cachix.nix
+++ nixos/system/cachix.nix
@@ -1,8 +1,12 @@
-{ pkgs, ...}: 
+{ pkgs, lib, ...}: 
 let
   users = import ../../config/users.nix;
 in {
-  nix.settings.trusted-users = [ "root" users.default.username ];
+  nix.settings = {
+    substituters = lib.mkAfter [ "https://g16-nixos.cachix.org" ];
+    trusted-public-keys = lib.mkAfter [ "g16-nixos.cachix.org-1:JlrHbD35SxKOpf6tnzTVTv6ZYMQYlIcipdXvj2C98Wc=" ];
+    trusted-users = lib.mkAfter [ "root" users.default.username ];
+  };
 
   environment.systemPackages = with pkgs; [
     cachix
